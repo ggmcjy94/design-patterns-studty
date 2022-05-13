16db511a27ec9bbfc021351d1993878f6f5fb7b7
“ggmcjy94” <lhs01071526524@gmail.com>
2022-05-13 11:47:03 +0900

    2022 05 - 13 prototype pattern commit add

---
+++ src/main/java/_01_creational_patterns/_04_builder/_02_after/App.java
@@ -0,0 +1,15 @@
+package _01_creational_patterns._04_builder._02_after;
+
+import _01_creational_patterns._04_builder._01_before.TourPlan;
+
+import java.time.LocalDate;
+
+public class App {
+
+    public static void main(String[] args) {
+        TourDirector director = new TourDirector(new DefaultTourBuilder());
+        TourPlan tourPlan = director.cancunTrip();
+        TourPlan longBeachTrip = director.longBeachTrip();
+
+    }
+}
---
+++ src/main/java/_01_creational_patterns/_05_prototype/_01_before/App.java
@@ -0,0 +1,9 @@
+package _01_creational_patterns._05_prototype._01_before;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
